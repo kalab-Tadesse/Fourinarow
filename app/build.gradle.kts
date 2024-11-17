ca5d2944e0446609b14873a9c25e4cef2be1909a
kalab-Tadesse <kalab7360@icloud.com>
2024-11-17 21:49:33 +0100

    Change of target Sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.fourinarow"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.fourinarow"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
